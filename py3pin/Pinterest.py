1782899497fa02d659398c1ccca85b3656ea8d38
zhonglj2012@gmail.com <zhonglj2012@gmail.com>
2020-12-23 15:05:04 +1100

    test

--- py3pin/Pinterest.py
+++ py3pin/Pinterest.py
@@ -135,7 +135,11 @@ class Pinterest:
             http_proxy.ssl_proxy = proxy
             http_proxy.add_to_capabilities(chrome_options)
 
-        driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        driver = webdriver.Chrome('chromedriver',chrome_options=chrome_options)
         driver.get("https://pinterest.com/login")
 
         try:
